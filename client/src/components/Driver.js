6a172a83d49286a1e528f72ab8e5ec48a31b752e
ravish0007 <ravish.nfs@gmail.com>
2022-06-15 20:21:22 +0530

    Add Driver.js: keeps sending device location to test marker movement

---
+++ client/src/components/Driver.js
@@ -0,0 +1,52 @@
+import { useEffect, useState, useRef } from 'react'
+import io from 'socket.io-client'
+
+const socket = io.connect('https://xpve.rocks:3001')
+
+export default function Driver () {
+  const [data, setData] = useState([])
+  const position = useRef(null)
+
+  useEffect(() => {
+    socket.on('receive_location', (data) => {
+      setData(x => x.concat(data))
+    })
+  }, [])
+
+  useEffect(() => {
+    sendLocation()
+    // setInterval(sendLocation, 2000)
+  }, [])
+
+  const handlePosition = (pos) => {
+    console.log('here')
+
+    const lat = pos.coords.latitude
+    const lng = pos.coords.longitude
+
+    if (position.current === null) {
+      position.current = { lng, lat }
+      return
+    }
+
+    if (position.current.lat === lat && position.current.lng === lng) {
+      return
+    }
+
+    position.current.lat = lat
+    position.current.lng = lng
+
+    socket.emit('send_location', { lat, lng })
+  }
+
+  const sendLocation = () => {
+    // return navigator.geolocation.getCurrentPosition(handlePosition, () => console.log('error at geolocation'))
+    return navigator.geolocation.watchPosition(handlePosition, () => console.log('error at geolocation'))
+  }
+
+  return (
+    <>
+      {data.map(blob => { return (<p> {JSON.stringify(blob)} </p>) })}
+    </>
+  )
+}
