c6a7f60c07bf62c5733df562813a72859cc1ece6
ravish0007 <ravish.nfs@gmail.com>
2022-06-15 20:23:33 +0530

    Add separate route for Map and Driver in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,14 @@
-import React, { useRef, useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router'
 
-import mapboxgl from 'mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
-
-mapboxgl.accessToken = 'pk.eyJ1Ijoidml2ZWswMDkiLCJhIjoiY2wxb3NsbWR5MDEwZjNpcHQ5aHpzOGs2bSJ9.hu2KGr7-tFgdBxyCTf_HkA'
+import Map from './components/Map'
+import Driver from './components/Driver'
 
 export default function App () {
-  const mapContainer = useRef(null)
-  const tooltipContainer = useRef(null)
-  const map = useRef(null)
-
-  const [lng, setLng] = useState(-70.9)
-  const [lat, setLat] = useState(42.35)
-  const [zoom, setZoom] = useState(15)
-
-  const handlePosition = (position) => {
-    const latitude = position.coords.latitude
-    const longitude = position.coords.longitude
-
-    setLng(longitude)
-    setLat(latitude)
-
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [longitude, latitude],
-      zoom: zoom
-    })
-  }
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(handlePosition, () => console.log('error at geolocation'))
-  }, [])
-
-  useEffect(() => {
-    if (map.current) return // initialize map only once
-
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    })
-
-    // const from = new mapboxgl.Marker(tooltipContainer).setLngLat([lng, lat]).addTo(map)
-  })
-
   return (
-    <div>
-      <div ref={mapContainer} className='map-container' />
-    </div>
+    <Routes>
+      <Route path='/' element={<Map />} />
+      <Route path='/driver' element={<Driver />} />
+    </Routes>
+
   )
 }
